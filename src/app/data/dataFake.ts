57d01f9db6657a7b14717c93923b6f1b6d64bb56
Regiane Gaspar <123254220+reggaspar@users.noreply.github.com>
2023-10-07 16:13:29 -0300

    projeto

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"Novo homem de ferro em 3d",
+        "description":"Marvel anuncia um novo filme do homem de ferro, confira",
+        "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/AD72115B9CDAE873B2234BD0908CEF7B45A2D9AD9C66C699239B46E582F70A8B/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    },
+    {
+        "id":"2",
+        "title":"Nova série anunciada no Disney +",
+        "description":"Algo aqui",
+        "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    }
+]
